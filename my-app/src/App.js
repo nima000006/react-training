d1a2dfc433a15f15e6d86377123ca63962e9b86b
nima <nimachehreh690@gmail.com>
2024-01-09 00:46:30 +0330

    delete function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,57 @@
+import React, { useState } from "react";
 
 const App = () => {
-    
-}
+    const [myArray, setMyArray] = useState([
+        {
+          name: "nima",
+          age: 26,
+          email: "nima@nima.com",
+          id: "1",
+        },
+        {
+          name: "jacob",
+          age: 16,
+          email: "jacob@jacob.com",
+          id: "2",
+        },
+        {
+          name: "lale",
+          age: 24,
+          email: "lale@lale.com",
+          id: "3",
+        },
+      ])
+  const deleteHandler = (id) => {
+      const updateArray = myArray.filter((item) => item.id !== id);
+      setMyArray(updateArray)
+  };
+  return (
+    <>
+      <table style={{ margin: "10px auto", width: "70%", textAlign: "center" }}>
+        <tr>
+          <th>id</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Email</th>
+          <th>Delete button</th>
+        </tr>
+
+        {myArray.map((item) => {
+          return (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.email}</td>
+              <td>
+                <button onClick={() => deleteHandler(item.id)}>delete</button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </>
+  );
+};
 
 export default App;
